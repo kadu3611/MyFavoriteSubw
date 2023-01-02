b75dc6306b6d8538ccc90834ea50868ae340e53f
Carlos Eduardo Fernandes Martins Salomao <kaduzeiro2013@gmail.com>
2023-01-02 14:55:58 -0300

    mudança no local das pastas e arquivos firebase.json, .firebase e .gitignore

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import HomePage from './HomePage';
+import HomePage from './App';
 import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import ProviderComponents from './context/ProviderComponents';
