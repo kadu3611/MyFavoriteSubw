f8dfd4013813f86af8d13d283978a61964c24b10
Carlos Eduardo Fernandes Martins Salomao <kaduzeiro2013@gmail.com>
2023-01-02 16:34:56 -0300

    REfeito a config do firebase para uma nova database e feito o deploy

--- src/Firebase/firebase-config.js
+++ src/Firebase/firebase-config.js
@@ -14,13 +14,3 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig)
 
 export const db = getFirestore(app)
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAs-oxK-4UX3CbyOZTLhrYS1tDddLinMZE",
-//   authDomain: "myfavoritesubway2.firebaseapp.com",
-//   projectId: "myfavoritesubway2",
-//   storageBucket: "myfavoritesubway2.appspot.com",
-//   messagingSenderId: "492061178741",
-//   appId: "1:492061178741:web:1867b3cd149f3def87713c",
-//   measurementId: "G-00SPF2E241"
-// };
